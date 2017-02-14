b513c147e17c5d6cacf5bb76bfbca6c8408e2189
ben <benoit.works@gmail.com>
2017-02-14 16:38:39 +0100

    angularjs version

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
 var STORAGE_KEY = 'postits'
 var storage = {
-  fetch: function () {
-    var postits = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
-    storage.uid = postits.length ;
-    return postits ;
-  },
-  save: function (postits) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(postits)) ;
-  }
+	fetch: function () {
+		var postits = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+		storage.uid = postits.length ;
+		console.log(postits);
+		return postits ;
+	},
+	save: function (postits) {
+		localStorage.setItem(STORAGE_KEY, angular.toJson(postits)) ;
+	}
 }
 
-var app = new Vue({
-	el: '.container',
-	data: { 
-		postits: storage.fetch()
-	},
-	watch: {
-		postits: {
-			handler: function (val, oldVal) {
-				console.log('postit changed from'+oldVal+" to "+val) ;
-				storage.save(this.postits);
-			},
-			deep: true
-		}
-	},
-	methods: {
-		removePostit: function (postit) {
-			this.postits.splice(this.postits.indexOf(postit), 1) ; 
-		},
-		addNewPostit : function () {
-			this.postits.push({
-				id: this.postits.length,
-				content: "new postit"
-			});
-		},
-	}
+angular.module('app', []).controller('postitController', function($scope) {
+	$scope.postits = storage.fetch();
+
+	$scope.addPostit = function() {
+		$scope.postits.push({id:100,content:"foobar"});
+		storage.save($scope.postits);
+	};
+
+	$scope.removePostit = function(postit) {
+		console.log(postit);
+		$scope.postits.splice($scope.postits.indexOf(postit), 1) ; 
+		storage.save($scope.postits);
+	};
+
+	$scope.$watch('postits', function (newVal, oldVal) {
+		storage.save($scope.postits);
+	}, true);
+
 });
+
 setTimeout(function() {
 	$("#loader").css("display","none");
 	$(".container").css("display","inherit");
